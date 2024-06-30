d7a84b34f521697f2d91d14cb85f692d5f9c5323
mckoyd <dominick.mckoy@gmail.com>
2024-06-30 09:31:32 -0400

    adds displayLikes method

---
+++ basicPartTwo/displayLikes/displayLikes.ts
@@ -0,0 +1,18 @@
+export const displayLikes = (arr: string[]): string => {
+  const arrLength = arr.length;
+
+  switch (arrLength) {
+    case 0:
+      return "no one likes this";
+    case 1:
+      return `${arr[0]} likes this`;
+    case 2:
+      return `${arr[0]} and ${arr[1]} like this`;
+    case 3:
+      return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+    default:
+      return `${arr[0]}, ${arr[1]} and ${arrLength - 2} others like this`;
+  }
+};
+
+displayLikes(["Alex", "Jacob", "Mark", "Max"]);
