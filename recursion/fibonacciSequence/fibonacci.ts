224e1e6d22fa583b421612711c025ad53a531dae
mckoyd <dominick.mckoy@gmail.com>
2024-06-30 18:22:12 -0400

    refactors fibonacci

--- recursion/fibonacciSequence/fibonacci.ts
+++ recursion/fibonacciSequence/fibonacci.ts
@@ -1,7 +1,2 @@
-export const fibonacci = (num: number): number => {
-  if (num < 2) {
-    return num;
-  }
-
-  return fibonacci(num - 2) + fibonacci(num - 1);
-};
+export const fibonacci = (num: number): number =>
+  num < 2 ? num : fibonacci(num - 1) + fibonacci(num - 2);
