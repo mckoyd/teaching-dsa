9433fac7cb773ff270d81fc31b7b04ce28ba179c
mckoyd <dominick.mckoy@gmail.com>
2024-06-29 21:37:34 -0400

    refactors getArrayIntersection method to use a set

--- basicPartTwo/getArrayIntersection/getArrayIntersection.ts
+++ basicPartTwo/getArrayIntersection/getArrayIntersection.ts
@@ -1,15 +1,8 @@
 export const getArrayIntersection = (arr1: number[], arr2: number[]) => {
   const intersectionArr: number[] = [];
 
-  const arr1Map = [...new Set(arr1)].reduce<Record<string, boolean>>(
-    (acc, curr) => {
-      acc[curr] = true;
-      return acc;
-    },
-    {}
-  );
-
-  arr2.forEach((number) => arr1Map[number] && intersectionArr.push(number));
+  const arr1Map = new Set(arr1);
+  arr2.forEach((number) => arr1Map.has(number) && intersectionArr.push(number));
 
   return intersectionArr;
 };
