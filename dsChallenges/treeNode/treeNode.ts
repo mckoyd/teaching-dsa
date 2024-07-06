a9c6ece35dc8cb1150bac8b7e89f69a1d7e245fc
mckoyd <dominick.mckoy@gmail.com>
2024-07-06 16:21:42 -0400

    adds treeNode class

---
+++ dsChallenges/treeNode/treeNode.ts
@@ -0,0 +1,11 @@
+export class TreeNode {
+  value: any;
+  left: null | TreeNode;
+  right: null | TreeNode;
+
+  constructor(value: any) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
